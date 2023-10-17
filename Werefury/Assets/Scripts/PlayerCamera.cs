9bf8ff7bb7c59da40c91c45fd63e6116a56943cd
fungusflip <81601975+fungusflip@users.noreply.github.com>
2023-10-17 11:47:50 +0200

    CameraScript,Car script(Not finished).

---
+++ Werefury/Assets/Scripts/PlayerCamera.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerCamera : MonoBehaviour
+{
+    [SerializeField] private Vector3 offset = new Vector3(0f, 0f, 0f);
+    [SerializeField] private GameObject cameraObject;
+    private Transform cameraPos;
+    private Vector3 currentVelocity;
+    [SerializeField] private float smoothTime = 0.3f;
+
+    void Start()
+    {
+        cameraPos = cameraObject.GetComponent<Transform>();
+    }
+
+    void LateUpdate()
+    {
+        Vector3 newPosition = transform.position + offset;
+        cameraPos.position = newPosition;
+
+        cameraObject.transform.position = Vector3.SmoothDamp(
+            cameraObject.transform.position,
+            new Vector3(cameraPos.position.x, cameraPos.position.y, cameraObject.transform.position.z),
+            ref currentVelocity,
+            smoothTime
+        );
+    }
+}
+
