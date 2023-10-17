fa437360d30917ec9cdb41ac7c4fc8adedf92e3c
fungusflip <filip.renemark@student.forsbergsskola.se>
2023-10-17 16:00:34 +0200

    Ordning och reda. + functioning CameraScript.

--- Werefury/Assets/Scripts/PlayerScripts/PlayerCamera.cs
+++ Werefury/Assets/Scripts/PlayerScripts/PlayerCamera.cs
@@ -1,34 +1,27 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerCamera : MonoBehaviour
 {
     [SerializeField] private Vector3 offset = new Vector3(0f, 0f, 0f);
-    private GameObject cameraObject;
+    [SerializeField] private GameObject cameraObject;
     private Transform cameraPos;
     private Vector3 currentVelocity;
     [SerializeField] private float smoothTime = 0.3f;
 
-    void Start()
+    void Awake()
     {
-        
-        cameraPos = cameraObject.GetComponent<Transform>();
+        GameObject CameraPosObject = GameObject.Find("CameraPos");
+        cameraPos = CameraPosObject.transform;
     }
-
-
-    public void FindPlayer()
-    {
-        cameraObject = GameObject.Find("Player");
-    }
-    void LateUpdate()
+    
+    void Update()
     {
         Vector3 newPosition = transform.position + offset;
         cameraPos.position = newPosition;
 
         cameraObject.transform.position = Vector3.SmoothDamp(
             cameraObject.transform.position,
-            new Vector3(cameraPos.position.x, cameraPos.position.y, cameraObject.transform.position.z),
+            new Vector3(cameraPos.position.x, cameraPos.position.y, cameraPos.position.z),
             ref currentVelocity,
             smoothTime
         );
