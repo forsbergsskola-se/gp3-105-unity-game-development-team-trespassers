42637909bc451fa140b0930887bdf70eb88b6872
fungusflip <filip.renemark@student.forsbergsskola.se>
2023-10-19 13:02:18 +0200

    Update ItemPicture.cs

--- Werefury/Assets/Scripts/Items/ItemPicture.cs
+++ Werefury/Assets/Scripts/Items/ItemPicture.cs
@@ -4,5 +4,5 @@ using UnityEngine;
 
 public class ItemPicture : MonoBehaviour
 {
-
+    //TODO ADD REFERENCE TO WHAT GUN NUMBER IN ITEMS AND PORTRAY IN HAND 1 OR HAND TWO AS A GAMEBOBJECT WHICH IS CHANGABLE
 }
