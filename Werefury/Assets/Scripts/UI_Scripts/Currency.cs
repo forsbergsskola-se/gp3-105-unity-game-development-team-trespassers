f8201c184179759adfc84c36f616e816339885c7
AbdulakhKurbanov <lankurbanov@gmail.com>
2023-10-20 17:22:43 +0200

    CreatingCurrencyDisplayer

---
+++ Werefury/Assets/Scripts/UI_Scripts/Currency.cs
@@ -0,0 +1,12 @@
+using UnityEngine;
+using UnityEngine.UI;
+namespace UI_Scripts
+{
+    public class Currency : MonoBehaviour
+    {
+        public int currency;
+        public Image money;
+        public Text amount;
+        
+    }
+}
