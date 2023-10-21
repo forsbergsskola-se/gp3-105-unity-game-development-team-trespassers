a825f319732a28ca877e97ff4b95878dc65a2924
AbdulakhKurbanov <lankurbanov@gmail.com>
2023-10-21 05:04:52 +0200

    CitizenAndCurrencyUpdated
    Citizen is now dropping x amount of money upon death and the amount is going to Currency displayed on screen.

--- Werefury/Assets/Scripts/NPC_Scripts/Citizen.cs
+++ Werefury/Assets/Scripts/NPC_Scripts/Citizen.cs
@@ -1,9 +1,14 @@
+using UI_Scripts;
 using UnityEngine;
-
+//This Citizen should have some money to drop when killed.
 public class Citizen : MonoBehaviour
 {
     [SerializeField] private float maxHealth = 10f;
     [SerializeField] private float health;
+    [SerializeField] private int minMoney =10;
+    [SerializeField] private int maxMoney =50;
+
+    public Currency currency;
     void Start()
     {
         health = maxHealth;
@@ -21,8 +26,15 @@ public class Citizen : MonoBehaviour
         if (health <=0)
         {
             Debug.Log("A Citizen died");
+            CitizenMoney();
             Destroy(gameObject);
-            
         }
+    }
+    public void CitizenMoney()
+    {
+        int moneyDrop = Random.Range(minMoney, maxMoney + 1);
+        Debug.Log(moneyDrop + "$ Dropped");
+        currency.currency += moneyDrop;
+        currency.UpdateCurrency(); 
     }
 }
--- Werefury/Assets/Scripts/UI_Scripts/Currency.cs
+++ Werefury/Assets/Scripts/UI_Scripts/Currency.cs
@@ -1,10 +1,25 @@
-using System;
 using UnityEngine;
 using UnityEngine.UI;
 namespace UI_Scripts
 {
     public class Currency : MonoBehaviour
     {
-        
+        public Text currencyAmount;
+        public int currency;
+
+        private void Start()
+        {
+            currency = 0;
+        }
+
+        private void Update()
+        {
+            UpdateCurrency();
+        }
+        public void UpdateCurrency()
+        {
+            currencyAmount.text = "$$$: " +currency;
+            
+        }
     }
 }
