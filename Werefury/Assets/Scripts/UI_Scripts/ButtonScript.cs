16c3b2aa9b11c889ebe26a1abbbe7b5a1ec770dd
AbdulakhKurbanov <lankurbanov@gmail.com>
2023-10-23 18:22:59 +0200

    MainMenuComplete

--- Werefury/Assets/Scripts/UI_Scripts/ButtonScript.cs
+++ Werefury/Assets/Scripts/UI_Scripts/ButtonScript.cs
@@ -1,22 +1,15 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class ButtonScript : MonoBehaviour
 {
-    public Text playButton;
-
-    //Press G to drop main hand weapon.
-    //Press W to move forward.
-    //Press S to move backward.
-    //Press A to move left.
-    //Press D to move right.
-    //Press Space to brake.
-    //Press E to Enter or Exit.
-    //Aim with mouse cursor.
-    void Update()
+    public void LoadScene()
     {
-        
+        SceneManager.LoadScene("Environment");
+    }
+
+    public void Exit()
+    {
+        Application.Quit();
     }
 }
