407ab09752384520ea3839d44151235714b7010c
AbdulakhKurbanov <lankurbanov@gmail.com>
2023-10-23 16:07:27 +0200

    NewMainMenuScene

---
+++ Werefury/Assets/Scripts/UI_Scripts/ButtonScript.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ButtonScript : MonoBehaviour
+{
+    public Text playButton;
+
+    //Press G to drop main hand weapon.
+    //Press W to move forward.
+    //Press S to move backward.
+    //Press A to move left.
+    //Press D to move right.
+    //Press Space to brake.
+    //Press E to Enter or Exit.
+    //Aim with mouse cursor.
+    void Update()
+    {
+        
+    }
+}
