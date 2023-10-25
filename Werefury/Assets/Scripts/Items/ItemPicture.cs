5594d7c1b020471093ba916ec786f296d7a97317
fungusflip <filip.renemark@student.forsbergsskola.se>
2023-10-25 13:49:53 +0200

    Enviroment 3 WeaponId and ItemPicture.

--- Werefury/Assets/Scripts/Items/ItemPicture.cs
+++ Werefury/Assets/Scripts/Items/ItemPicture.cs
@@ -1,9 +1,46 @@
+using System;
 using UnityEngine;
 
 namespace Items
 {
     public class ItemPicture : MonoBehaviour
     {
+        public GameObject player;
+       
+        [SerializeField] private Sprite[] weaponSprites;
+        
+        private Items items;
+        private Weapon weapon1;
+        private Weapon weapon2;
 
+         public void FixedUpdate()
+         {
+             GetComponents();
+         }
+
+         public void GetComponents()
+        {
+            this.items = player.GetComponent<Items>();
+            
+            if(items.HeldItem1 != null)
+            {
+                this.weapon1 = items.HeldItem1.GetComponent<Weapon>();
+                
+            }
+
+            if (items.HeldItem2 != null)
+            {
+                this.weapon2 = items.HeldItem2.GetComponent<Weapon>();
+            }
+            
+        }
+
+        private void ShowWeapon()
+        {
+            if (weapon1.weaponId == "1")
+            {
+                
+            }
+        }
     }
 }
---
+++ Werefury/Assets/Scripts/Items/Weapon.cs
@@ -0,0 +1,8 @@
+using UnityEngine;
+
+public class Weapon : MonoBehaviour
+{
+    [SerializeField] public string weaponId;
+   
+
+}
