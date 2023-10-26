71784ccb320c5ef4b9d01d79f4b426815e41c36c
AbdulakhKurbanov <lankurbanov@gmail.com>
2023-10-26 17:09:18 +0200

    RunNPCRun

---
+++ Werefury/Assets/Scripts/NPC_Scripts/PassiveNPC.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+public class PassiveNPC : MonoBehaviour
+{
+    public float moveSpeed = 3.0f;
+    public float movementRange = 5.0f;
+    public float changeDirectionTime = 2.0f;
+    private float timer = 0.0f;
+    private Vector3 randomDirection;
+    
+    
+    void Start()
+    {
+        GenerateRandomDirection();
+    }
+    
+    void Update()
+    {
+        timer += Time.deltaTime;
+        if (timer >= changeDirectionTime)
+        {
+            GenerateRandomDirection();
+            timer = 0.0f;
+        }
+
+        // Move the NPC
+        transform.Translate(randomDirection.normalized * moveSpeed * Time.deltaTime);
+    }
+    void GenerateRandomDirection()
+    {
+        float randomX = Random.Range(-movementRange, movementRange);
+        float randomZ = Random.Range(-movementRange, movementRange);
+        randomDirection = new Vector3(randomX, 0, randomZ);
+    }
+}
