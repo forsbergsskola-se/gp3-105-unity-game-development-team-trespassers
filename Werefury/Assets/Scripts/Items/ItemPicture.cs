85f54d31c29fe817b48a8a54577247e41378b78a
fungusflip <filip.renemark@student.forsbergsskola.se>
2023-10-30 02:52:40 +0100

    Fixed DualHand ui, Shooting error,

--- Werefury/Assets/Scripts/Items/ItemPicture.cs
+++ Werefury/Assets/Scripts/Items/ItemPicture.cs
@@ -7,6 +7,7 @@ namespace Items
     public class ItemPicture : MonoBehaviour
     {
         public Image weieldImage;
+        public Image weieldImage2;
         public GameObject player;
 
         [SerializeField] private Sprite[] weaponSprites;
@@ -19,12 +20,20 @@ namespace Items
         {
             weieldImage = GetComponent<Image>();
             weieldImage.sprite = null;
+            weieldImage2.sprite = null;
+            Color weieldImageColor = weieldImage.color;
+            weieldImageColor.a = 0f;
+            weieldImage.color = weieldImageColor; 
+            Color weieldImageColor2 = weieldImage.color;
+            weieldImageColor2.a = 0f;
+            weieldImage2.color = weieldImageColor; 
         }
 
         public void FixedUpdate()
         {
             GetComponents();
-            ShowWeapon();
+            ShowWeapon1();
+            ShowWeapon2();
         }
 
         public void GetComponents()
@@ -34,33 +43,85 @@ namespace Items
             if (items.HeldItem1 != null)
             {
                 this.weapon1 = items.HeldItem1.GetComponent<Weapon>();
-
+            }
+            else
+            {
+                weapon1 = null;
             }
 
             if (items.HeldItem2 != null)
             {
                 this.weapon2 = items.HeldItem2.GetComponent<Weapon>();
             }
-
+            else
+            {
+                weapon2 = null;
+            }
         }
 
-        private void ShowWeapon()
+        private void ShowWeapon1()
         {
+            Color weieldImageColor = weieldImage.color;
+
             if (weapon1 != null)
             {
                 switch (weapon1.weaponId)
                 {
                     case "1":
-                        weieldImage.sprite = weaponSprites[1];
+                        weieldImage.sprite = weaponSprites[0];
+                        weieldImageColor.a = 1f;
+                        weieldImage.color = weieldImageColor; 
                         break;
                     case "2":
-                        weieldImage.sprite = weaponSprites[2];
+                        weieldImage.sprite = weaponSprites[1];
+                        weieldImageColor.a = 1f;
+                        weieldImage.color = weieldImageColor; 
+                        break;
+                    default:
+                        weieldImage.sprite = null; 
+                        weieldImageColor.a = 0f;
+                        weieldImage.color = weieldImageColor; 
                         break;
                 }
             }
             else
             {
                 weieldImage.sprite = null;
+                weieldImageColor.a = 0f;
+                weieldImage.color = weieldImageColor; 
+            }
+        }
+        
+        private void ShowWeapon2()
+        {
+            Color weieldImageColor = weieldImage2.color;
+
+            if (weapon2 != null)
+            {
+                switch (weapon2.weaponId)
+                {
+                    case "1":
+                        weieldImage2.sprite = weaponSprites[0];
+                        weieldImageColor.a = 1f;
+                        weieldImage2.color = weieldImageColor; 
+                        break;
+                    case "2":
+                        weieldImage2.sprite = weaponSprites[1];
+                        weieldImageColor.a = 1f;
+                        weieldImage2.color = weieldImageColor; 
+                        break;
+                    default:
+                        weieldImage2.sprite = null; 
+                        weieldImageColor.a = 0f;
+                        weieldImage2.color = weieldImageColor; 
+                        break;
+                }
+            }
+            else
+            {
+                weieldImage2.sprite = null;
+                weieldImageColor.a = 0f;
+                weieldImage2.color = weieldImageColor; 
             }
         }
     }
