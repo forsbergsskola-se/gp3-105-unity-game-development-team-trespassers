fe96ccff33a43651c956e73c5f3724bd2804d4b0
AbdulakhKurbanov <lankurbanov@gmail.com>
2023-11-02 12:25:27 +0100

    NPCMovementAdjusted

--- Werefury/Assets/Scripts/NPC_Scripts/PassiveNPC.cs
+++ Werefury/Assets/Scripts/NPC_Scripts/PassiveNPC.cs
@@ -1,20 +1,19 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
+
 public class PassiveNPC : MonoBehaviour
 {
     public float moveSpeed = 3.0f;
     public float movementRange = 5.0f;
     public float changeDirectionTime = 2.0f;
+    public float avoidanceDistance = 2.0f; 
     private float timer = 0.0f;
     private Vector3 randomDirection;
-    
     
     void Start()
     {
         GenerateRandomDirection();
     }
-    
+
     void Update()
     {
         timer += Time.deltaTime;
@@ -24,9 +23,15 @@ public class PassiveNPC : MonoBehaviour
             timer = 0.0f;
         }
 
-        // Move the NPC
+        RaycastHit hit;
+        if (Physics.Raycast(transform.position, transform.forward, out hit, avoidanceDistance))
+        {
+            randomDirection = -hit.normal;
+        }
+
         transform.Translate(randomDirection.normalized * moveSpeed * Time.deltaTime);
     }
+
     void GenerateRandomDirection()
     {
         float randomX = Random.Range(-movementRange, movementRange);
